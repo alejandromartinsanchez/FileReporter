adf56a9f071d7f9049810201774cf7958f726b31
Alejandro Martin Sanchez <alejandro3dimensiones@gmail.com>
2023-01-29 05:05:39 -0800

    First Steps

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
 ﻿namespace FileReporter
+
+    /*
+      * Collect all files and folders for a given drive (First enumerate all drives and then ask the usuary about the drive he want to analyze)
+       * Analyze all folders, subfolders and files inside and collect:
+		* File size
+		* File extension*/
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //This is a test commentary.           
+            Console.WriteLine("List of all drives:");
+            DriveInfo[] drives = DriveInfo.GetDrives();           
+            Console.WriteLine(drives[0].Name + " " + drives[1].Name);
+            Console.WriteLine("Choose the drive you want to analyze.");
+
+
+
         }
     }
 }
